174229236115bc4601d8e4d56634e123bff28c79
MouhamadouFALL <mouhamadou2.fall@gmail.com>
2022-02-27 21:01:58 +0000

    les manières d'ajouter de nouvelles propriètés

--- chap02/02_01/script.js
+++ chap02/02_01/script.js
@@ -1,16 +1,23 @@
 
 // metode 1 pour créer un objet
-/*let employee = {
+let employee = {
     firstName : "Ngor",
     lastName : "FAYE",
     age : 28
-};*/
-
-// metode 2 pour créer un objet (annotation litérale plus populaire)
-let employee = new Object();
-employee.firstName = "Ngor";
-employee.lastName = "FAYE";
-employee.age = 28;
+};
 
 // Affiche l'Objet sur la console
+console.log(employee);
+
+// méthode 1 Ajouter une nouvelle proprièté
+employee.email = "ngor.faye@gmail.com";
+console.log(employee);
+
+// méthode 2 Ajouter une nouvelle proprièté
+employee['country'] = "Senegal";
+console.log(employee);
+
+// méthode 3 Ajouter une nouvelle proprièté (dynamique)
+let prop = 'telephone';
+employee[prop] = '+221778887777';
 console.log(employee);
