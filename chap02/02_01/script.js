0a123d6780a887a2f6509f151aed804246978bca
MouhamadouFALL <mouhamadou2.fall@gmail.com>
2022-02-27 20:42:24 +0000

    Appréhender les methodes de construction d'Oject en JavaScript

---
+++ chap02/02_01/script.js
@@ -0,0 +1,16 @@
+
+// metode 1 pour créer un objet
+/*let employee = {
+    firstName : "Ngor",
+    lastName : "FAYE",
+    age : 28
+};*/
+
+// metode 2 pour créer un objet (annotation litérale plus populaire)
+let employee = new Object();
+employee.firstName = "Ngor";
+employee.lastName = "FAYE";
+employee.age = 28;
+
+// Affiche l'Objet sur la console
+console.log(employee);
