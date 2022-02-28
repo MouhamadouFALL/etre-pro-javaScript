f2e91a449529cbf10cdbf505426f18b114012b46
MouhamadouFALL <mouhamadou2.fall@gmail.com>
2022-02-28 10:32:51 +0000

    appréhender quelques constructeur de javaScript

---
+++ 03/script02.js
@@ -0,0 +1,18 @@
+
+// Constructeur de javaScript
+let employee = new Object();
+employee.nom = "Ngor";
+
+let today = new Date()
+let birthDay = new Date('14 Aug 1994');
+
+let erreur =  new Error();
+let msgErr = new Error("Il y'a une erreur !")
+
+
+console.log(employee);
+console.log(today);
+console.log(birthDay);
+console.log(erreur);
+console.log(msgErr);
+
