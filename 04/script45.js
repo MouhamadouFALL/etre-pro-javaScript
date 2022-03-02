45e9c2d155fbfe9f062f0a0123dba40acff3ea15
MouhamadouFALL <mouhamadou2.fall@gmail.com>
2022-03-02 16:29:04 +0000

    Afficher la Description des propriétés du prototype

---
+++ 04/script45.js
@@ -0,0 +1,25 @@
+
+function Employee(firstName, lastName, country='Senegal'){
+
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.country = country;
+
+}
+
+Employee.prototype.sayHello = function (){
+    return `${this.firstName} say welcome!`;
+}
+
+let employee = new Employee('Leukk', 'Ndour', 'Mali');
+// afficher la description des propriètés
+console.log(Object.getOwnPropertyDescriptor(Employee.prototype, 'sayHello'));
+console.log(Object.getOwnPropertyDescriptor(Employee.prototype, 'constructor'));
+console.log();
+
+console.log(employee);
+
+// connaitre les de employee auxquels on a accés
+for (prop in employee){
+    console.log(prop);
+}
