ce94e0a6cb269f7ddc2b26206c8f78a991f99644
MouhamadouFALL <mouhamadou2.fall@gmail.com>
2022-03-03 16:50:05 +0000

    référencer les constructeurs

--- 05/script51.js
+++ 05/script51.js
@@ -11,20 +11,22 @@ Person.prototype.sayHello = function(){
 
 
 function Employee(firstName, lastName, country="Sunugaal", baseSalary=1500){
-    
+    Person.call(this, firstName, lastName, country);
     this.baseSalary= baseSalary;
 }
 
 function Customer(firstName, lastName, customerId, country="Sunugaal"){
-    
+    Person.apply(this, [firstName, lastName, country="Sunugaal"]);
     this.customerId = customerId;
 }
 
 Employee.prototype = new Person();
+Employee.prototype.constructor = Employee;
 Customer.prototype = new Person();
+Customer.prototype.constructor = Customer;
 
 let employee = new Employee('Boukki', 'Ndour');
 let customer = new Customer('Leukk', 'Diagne', 00001);
 
-console.log(employee.sayHello());
-console.log(customer.sayHello());
+console.log(employee.constructor);
+console.log(customer);
