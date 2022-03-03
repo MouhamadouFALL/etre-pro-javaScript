7411a65ffa5deebdacd0aed029822fdc1d4a9e70
MouhamadouFALL <mouhamadou2.fall@gmail.com>
2022-03-03 17:01:45 +0000

    factoriser la mise en place des constructeurs

---
+++ 05/script52.js
@@ -0,0 +1,36 @@
+
+function Person(firstName, lastName, country="Sunugaal"){
+    this.firstName = firstName;
+    this.lastName  = lastName;
+    this.country   = country;
+}
+
+Person.prototype.sayHello = function(){
+    return `${this.firstName} say Hello and welcome !`;
+}
+
+
+function Employee(firstName, lastName, country="Sunugaal", baseSalary=1500){
+    Person.call(this, firstName, lastName, country);
+    this.baseSalary= baseSalary;
+}
+
+function Customer(firstName, lastName, customerId, country="Sunugaal"){
+    Person.apply(this, [firstName, lastName, country="Sunugaal"]);
+    this.customerId = customerId;
+}
+
+
+function extend(Child, Parent){
+    Child.prototype = Object.create(Parent.prototype); //new Parent();
+    Child.prototype.constructor = Child;
+}
+
+extend(Employee, Person);
+extend(Customer, Person);
+
+let employee = new Employee('Boukki', 'Ndour');
+let customer = new Customer('Leukk', 'Diagne', 00001);
+
+console.log(employee.constructor);
+console.log(customer);
