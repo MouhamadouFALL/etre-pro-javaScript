b176cbf654886e5b934c4e554f51ba9b1ac5d128
MouhamadouFALL <mouhamadou2.fall@gmail.com>
2022-03-03 16:38:49 +0000

    Tester la methode sayHello()

--- 05/script51.js
+++ 05/script51.js
@@ -1,31 +1,30 @@
 
-function Person(){
-
+function Person(firstName, lastName, country="Sunugaal"){
+    this.firstName = firstName;
+    this.lastName  = lastName;
+    this.country   = country;
 }
 
 Person.prototype.sayHello = function(){
     return `${this.firstName} say Hello and welcome !`;
 }
 
+
 function Employee(firstName, lastName, country="Sunugaal", baseSalary=1500){
-    this.firstName = firstName;
-    this.lastName  = lastName;
-    this.country   = country;
+    
     this.baseSalary= baseSalary;
 }
 
 function Customer(firstName, lastName, customerId, country="Sunugaal"){
-    this.firstName = firstName;
-    this.lastName  = lastName;
-    this.country   = country;
+    
     this.customerId = customerId;
 }
 
 Employee.prototype = new Person();
-Customer.prototype = new Customer();
+Customer.prototype = new Person();
 
 let employee = new Employee('Boukki', 'Ndour');
 let customer = new Customer('Leukk', 'Diagne', 00001);
 
-console.log(employee);
-console.log(customer);
+console.log(employee.sayHello());
+console.log(customer.sayHello());
